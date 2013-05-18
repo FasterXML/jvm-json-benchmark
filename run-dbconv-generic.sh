b2c0ae21e011578949448ed7e733b07bab7229d4
Tatu Saloranta <tsaloranta@gmail.com>
2013-05-18 14:58:13 -0700

    add default cfgs (need to clean up later)

---
+++ run-dbconv-generic.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+java -cp lib/\* \
+ -Djapex.inputDir=data/db-medium \
+ -Djapex.warmupTime=5 \
+ -Djapex.runTime=15 \
+ -Djapex.warmupsPerDriver=1 \
+ -Djapex.runsPerDriver=1 \
+ com.sun.japex.Japex testcfg/dbconv-generic.xml
+
---
+++ run-dbconv-json.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+ 
+echo "About to run full 'dbconv' test on JSON drivers, medium data set"
+
+# Nothing big stored in memory, heap can remain modest 
+# -Djapex.runTime=30 \
+java -server -cp lib/\* \
+ -Xmx128M \
+ -Djapex.runsPerDriver=3 \
+ -Djapex.warmupTime=5 \
+ -Djapex.runTime=30 \
+ -Djapex.numberOfThreads=1 \
+ -Djapex.reportsDirectory=japex-reports \
+ -Djapex.inputDir=data/db-medium \
+ com.sun.japex.Japex \
+ testcfg/dbconv-json.xml
+
+echo "Done!";
